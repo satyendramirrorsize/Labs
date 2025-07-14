880da485632b6df863f015ffc9d8b9a93a0fb142
satykumar36 <satykumar36@gmail.com>
2025-07-15 00:27:51 +0530

    Development Process Section added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  initParticles("particle-js-2");
+  initParticles("particle-js-6");
+});
+
 const jobsUrl = "./data/jobData.json";
 
 fetch(jobsUrl)
@@ -53,7 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
   particlesJS("particle-js", {
     particles: {
       number: {
-        value: 80,
+        value: 100,
         density: {
           enable: true,
           value_area: 800,
@@ -163,8 +169,8 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  particlesJS("particle-js-2", {
+function initParticles(id) {
+  particlesJS(id, {
     particles: {
       number: {
         value: 80,
@@ -185,52 +191,23 @@ document.addEventListener("DOMContentLoaded", () => {
         polygon: {
           nb_sides: 5,
         },
-        image: {
-          src: "img/github.svg",
-          width: 100,
-          height: 100,
-        },
       },
       opacity: {
         value: 0.5,
         random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false,
-        },
       },
       size: {
         value: 3,
         random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false,
-        },
       },
       line_linked: {
-        enable: false, // Disable the connecting lines
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1,
+        enable: false,
       },
       move: {
         enable: true,
         speed: 6,
         direction: "none",
-        random: false,
-        straight: false,
         out_mode: "out",
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200,
-        },
       },
     },
     interactivity: {
@@ -244,22 +221,8 @@ document.addEventListener("DOMContentLoaded", () => {
           enable: true,
           mode: "push",
         },
-        resize: true,
       },
       modes: {
-        grab: {
-          distance: 400,
-          line_linked: {
-            opacity: 1,
-          },
-        },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3,
-        },
         repulse: {
           distance: 200,
           duration: 0.4,
@@ -267,14 +230,12 @@ document.addEventListener("DOMContentLoaded", () => {
         push: {
           particles_nb: 4,
         },
-        remove: {
-          particles_nb: 2,
-        },
       },
     },
     retina_detect: true,
   });
-});
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   particlesJS("particle-js-3", {
